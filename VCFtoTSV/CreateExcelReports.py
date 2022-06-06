0acba2addefa86453f9767c5f0bb61d2d9628517
Matthew Wells <matthew.wells@canada.ca>
2022-06-06 14:16:03 -0500

    added basic excel report functionality

--- VCFtoTSV/CreateExcelReports.py
+++ VCFtoTSV/CreateExcelReports.py
@@ -15,6 +15,7 @@ import pandas as pd
 
 # Nice python library 
 import os
+import glob
 
 
 
@@ -22,17 +23,45 @@ class HTMLToExcel:
     """
     Read all html tables in a directory and covert to a single excel file
     """
+    _glob_word_ = "test" # the word terminating the html plots generated currently
     def __init__(self, directory_recurse) -> None:
+        self.html_voc_data = {}
         self.directory_recurse = directory_recurse
-    
-    def read_html_file(self):
+        self.directories = self.recurse_directory()
+        [self.read_html_file(i) for i in self.directories] # map call wasnt working for somereason
+
+    def read_html_file(self, fp):
         """
         Read html file and 
         """
-        df = pd.read_html("./tests/Delta_test.html")
-        print(type(df))
+        #fp = f"./tests/Delta_{self._glob_word_}.html"
+        sample_name = os.path.basename(fp)[:os.path.basename(fp).index("_")]
+        df = pd.read_html(fp)
+        samples = [i for i in list(df[0].columns)[1:]]
+        df[0].rename(columns={"Unnamed: 0": "NucName+AAName"}, inplace=True)
+        df[0]["AAName"], df[0]["NucName"] = zip(*df[0]["NucName+AAName"].apply(lambda x: x.split("|"))) # create split columns for indexing
+        df[0]["VOC"] = sample_name
+        df[0]["Position"] = df[0]["NucName"].apply(lambda x: int(''.join([i for i in x if i.isdigit()])))
+        df[0] = df[0].reindex(columns=["VOC", "Position", "AAName", "NucName", "NucName+AAName", *samples])
+        self.html_voc_data[sample_name] = df[0] # update and maintain a worksheets in place
+        print(df[0].columns)
+        return True
+    
+    def recurse_directory(self):
+        """
+        Find all HTML files for voc data to be read in from.
+        """
+        glob_str = os.path.join(self.directory_recurse, f"*_{self._glob_word_}.html")
+        files = glob.glob(glob_str)
+        return files
+    
+    def html_dict_to_excel(self):
+        """
+        Take in the
+        """
+        ...
 
 
 if __name__=="__main__":
     xx = HTMLToExcel("./tests")
-    xx.read_html_file()
+    print(xx.html_voc_data.keys())
